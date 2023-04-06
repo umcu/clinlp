06fc4f9af88eb8b8e8e4a17c104b3379110059f8
Vincent Menger <vmenger@protonmail.com>
2023-04-06 16:15:27 +0200

    Add tokenizer tests

---
+++ tests/test_nlp.py
@@ -0,0 +1,10 @@
+from clinlp.clinlp import create_model
+
+
+class TestCreateModel:
+    def test_create_model(self):
+        _ = create_model()
+
+    def test_apply_model(self):
+        nlp = create_model()
+        nlp("dit is een test")
