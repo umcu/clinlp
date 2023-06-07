876a2cee0be7a589fd3eeb390c0d08a3d56b2ae3
Vincent Menger <vmenger@protonmail.com>
2023-06-07 16:18:09 +0200

    Add normalizer integration test

---
+++ tests/integration/normalizer.py
@@ -0,0 +1,25 @@
+import spacy
+
+import clinlp
+
+
+class TestNormalizerIntegration:
+
+    def test_normalizer_with_entity_matching(self):
+
+        nlp = spacy.blank('clinlp')
+        nlp.add_pipe('clinlp_normalizer')
+
+        ruler = nlp.add_pipe('entity_ruler', config={'phrase_matcher_attr': "NORM"})
+
+        terms = {
+            'symptomen': ['caries']
+        }
+
+        for term_description, terms in terms.items():
+            ruler.add_patterns([{'label': term_description, 'pattern': term} for term in terms])
+
+        doc = nlp("patient heeft veel last van cariës")
+
+        assert len(doc.ents) == 1
+        assert doc.ents[0].text == "cariës"
