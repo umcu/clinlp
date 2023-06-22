2b40f477694a94217df369ca53d267ec07bc2de3
Vincent Menger <vmenger@protonmail.com>
2023-06-22 09:32:13 +0200

    Formatting

--- clinlp/component/qualifier.py
+++ clinlp/component/qualifier.py
@@ -326,7 +326,7 @@ class ContextMatcher:
                 qualifiers = set()
 
                 for match_interval in match_scopes.overlap(ent.start, ent.end):
-                    if ent.start + 1 > match_interval.data.end or ent.end < match_interval.data.start + 1:
+                    if (ent.start + 1 > match_interval.data.end) or (ent.end < match_interval.data.start + 1):
                         qualifiers.add(str(match_interval.data.rule.qualifier))
 
                 ent._.set(QUALIFIERS_ATTR, qualifiers)
