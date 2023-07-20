f5a0af7ffc7e3e9d78974d277c6c6ca2de5c67c7
Vincent Menger <vmenger@protonmail.com>
2023-07-20 15:10:50 +0200

    Update to not use default for affirmed

--- clinlp/qualifier/transformer.py
+++ clinlp/qualifier/transformer.py
@@ -135,11 +135,8 @@ class NegationTransformer(QualifierDetector):
             prob = self._get_negation_prob(text, ent_start_char, ent_end_char, probas_aggregator=self.probas_aggregator)
 
             if prob > self.negation_threshold:
-                qualifier = self.negation_factory.get_qualifier("NEGATED", prob=prob)
-            elif 1-prob > self.affirmed_threshold:
-                qualifier = self.negation_factory.get_qualifier("AFFIRMED", prob=prob)
-            else:
-                qualifier = self.negation_factory.get_qualifier("UNKNOWN", prob=prob)
+                self.add_qualifier_to_ent(ent, self.negation_factory.get_qualifier("NEGATED", prob=prob))
+            elif prob > self.affirmed_threshold:
+                self.add_qualifier_to_ent(ent, self.negation_factory.get_qualifier("UNKNOWN", prob=prob))
 
-            self.add_qualifier_to_ent(ent, qualifier)
         return doc
