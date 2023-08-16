9012dffb5c958719673f7be12e03f32b61207a19
Vincent Menger <vmenger@protonmail.com>
2023-08-16 18:05:26 +0200

    Fixes

--- clinlp/ner.py
+++ clinlp/ner.py
@@ -9,13 +9,13 @@ from spacy.tokens import Span
 
 from clinlp.util import clinlp_autocomponent
 
-_defaults_clinlp_ner = {"attr": "TEXT", "proximity": 0, "fuzzy": 0, "fuzzy_min_len": float("inf"), "pseudo": False}
+_defaults_clinlp_ner = {"attr": "TEXT", "proximity": 0, "fuzzy": 0, "fuzzy_min_len": 0, "pseudo": False}
 
 
 @dataclass
 class Term:
     phrase: str
-    attr: str = (_defaults_clinlp_ner["attr"],)
+    attr: Optional[str] = _defaults_clinlp_ner["attr"]
     proximity: Optional[int] = _defaults_clinlp_ner["proximity"]
     fuzzy: Optional[int] = _defaults_clinlp_ner["fuzzy"]
     fuzzy_min_len: Optional[int] = _defaults_clinlp_ner["fuzzy_min_len"]
@@ -24,18 +24,19 @@ class Term:
     def to_spacy_pattern(self, nlp: Language):
         spacy_pattern = []
 
-        tokens = [token.text for token in nlp.tokenizer(self.phrase)]
+        phrase_tokens = [token.text for token in nlp.tokenizer(self.phrase)]
 
-        for token in tokens:
-            if (self.fuzzy > 0) and (len(token) < self.fuzzy_min_len):
+        for i, token in enumerate(phrase_tokens):
+            if (self.fuzzy > 0) and (len(token) >= self.fuzzy_min_len):
                 token_pattern = {f"FUZZY{self.fuzzy}": token}
             else:
                 token_pattern = token
 
             spacy_pattern.append({self.attr: token_pattern})
 
-            for _ in range(self.proximity):
-                spacy_pattern.append({"OP": "?"})
+            if i != len(phrase_tokens) - 1:
+                for _ in range(self.proximity):
+                    spacy_pattern.append({"OP": "?"})
 
         return spacy_pattern
 
@@ -47,51 +48,67 @@ class Term:
     default_config=_defaults_clinlp_ner,
 )
 @clinlp_autocomponent
-class ClinlpNer:
+class ClinlpNer(Term):
     def __init__(self, nlp: Language, **kwargs):
         self.nlp = nlp
+        self.attr = kwargs.get('attr', _defaults_clinlp_ner['attr'])
         self.term_defaults = kwargs
-        self.use_phrase_matcher = any(_defaults_clinlp_ner[k] != v for k, v in self.term_defaults.items())
 
         self._matcher = Matcher(self.nlp.vocab)
-        self._phrase_matcher = PhraseMatcher(self.nlp.vocab, attr=_defaults_clinlp_ner["attr"])
+        self._phrase_matcher = PhraseMatcher(self.nlp.vocab, attr=self.attr)
 
-        self.rules = {}
-        self.rule_labels = {}
+        self.terms = {}
+        self.term_concept = {}
 
-    def load_concepts(self, concepts: dict):
-        for concept, terms in concepts.items():
-            for term in terms:
-                identifier = str(len(self.rules))
+    @property
+    def use_phrase_matcher(self):
+        non_phrase_matcher_settings = ["proximity", "fuzzy", "fuzzy_min_len"]
 
-                self.rules[identifier] = term
-                self.rule_labels[identifier] = concept
+        for field in non_phrase_matcher_settings:
+            if field in self.term_defaults and self.term_defaults[field] != _defaults_clinlp_ner[field]:
+                return False
 
-                if isinstance(term, str):
+        return True
+
+    def load_concepts(self, concepts: str | dict):
+        for concept, concept_terms in concepts.items():
+            for concept_term in concept_terms:
+                identifier = str(len(self.terms))
+
+                self.terms[identifier] = concept_term
+                self.term_concept[identifier] = concept
+
+                if isinstance(concept_term, str):
                     if self.use_phrase_matcher:
-                        self._phrase_matcher.add(key=identifier, docs=[self.nlp(term)])
+                        self._phrase_matcher.add(key=identifier, docs=[self.nlp(concept_term)])
                     else:
-                        term = Term(term, **self.term_defaults).to_spacy_pattern(self.nlp)
-                        self._matcher.add(key=identifier, patterns=[term])
+                        concept_term = Term(concept_term, **self.term_defaults).to_spacy_pattern(self.nlp)
+                        self._matcher.add(key=identifier, patterns=[concept_term])
 
-                elif isinstance(term, list):
-                    self._matcher.add(key=identifier, patterns=[term])
+                elif isinstance(concept_term, list):
+                    self._matcher.add(key=identifier, patterns=[concept_term])
 
-                elif isinstance(term, Term):
-                    self._matcher.add(key=identifier, patterns=[term.to_spacy_pattern(self.nlp)])
+                elif isinstance(concept_term, Term):
+                    self._matcher.add(key=identifier, patterns=[concept_term.to_spacy_pattern(self.nlp)])
 
     def __call__(self, doc: Doc):
-        if len(self.rules) == 0:
+        if len(self.terms) == 0:
             return RuntimeError("No concepts added.")
 
-        matches = itertools.chain(self._matcher(doc), self._phrase_matcher(doc))
+        matches = []
+
+        if len(self._matcher) > 0:
+            matches += list(self._matcher(doc))
+
+        if len(self._phrase_matcher) > 0:
+            matches += list(self._phrase_matcher(doc))
 
         pos_matches = []
         neg_matches = ivt.IntervalTree()
 
         for match_id, start, end in matches:
             rule_id = self.nlp.vocab.strings[match_id]
-            term = self.rules[rule_id]
+            term = self.terms[rule_id]
 
             if isinstance(term, Term) and term.pseudo:
                 neg_matches[start:end] = rule_id
@@ -102,12 +119,12 @@ class ClinlpNer:
 
         for match_id, start, end in pos_matches:
             if any(
-                self.rule_labels[match_id] == self.rule_labels[neg_match_id.data]
+                self.term_concept[match_id] == self.term_concept[neg_match_id.data]
                 for neg_match_id in neg_matches.overlap(start, end)
             ):
                 continue
 
-            ents.append(Span(doc=doc, start=start, end=end, label=self.rule_labels[match_id]))
+            ents.append(Span(doc=doc, start=start, end=end, label=self.term_concept[match_id]))
 
         doc.set_ents(entities=ents)
 
