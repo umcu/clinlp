c6f44200764dc536d54a8fd68da9010449b14c66
Vincent Menger <vmenger@protonmail.com>
2024-06-03 14:11:29 +0200

    Update import order

--- src/clinlp/__init__.py
+++ src/clinlp/__init__.py
@@ -1,16 +1,14 @@
 import importlib.util
 
-# components
 from .ie.entity import RuleBasedEntityMatcher
 from .ie.qualifier import ContextAlgorithm
+from .language import Clinlp
+from .normalizer import Normalizer
+from .sentencizer import Sentencizer
 
 if importlib.util.find_spec("transformers") is not None:
     from .ie.qualifier import ExperiencerTransformer, NegationTransformer
 
-# base
-from .language import Clinlp
-from .normalizer import Normalizer
-from .sentencizer import Sentencizer
 
 __all__ = [
     "RuleBasedEntityMatcher",
