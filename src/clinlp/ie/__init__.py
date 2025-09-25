313962d96d5ca75fb1302b1edecc3a8f1a6bb840
Vincent Menger <vmenger@protonmail.com>
2025-09-25 09:58:02 +0200

    Remove unused import

--- src/clinlp/ie/__init__.py
+++ src/clinlp/ie/__init__.py
@@ -1,6 +1,6 @@
 """Information Extraction (IE) module for ``clinlp``."""
 
-from .entity import SPANS_KEY, RuleBasedEntityMatcher, create_concept_dict
+from .entity import SPANS_KEY, RuleBasedEntityMatcher
 from .term import Term
 
-__all__ = ["SPANS_KEY", "RuleBasedEntityMatcher", "Term", "create_concept_dict"]
+__all__ = ["SPANS_KEY", "RuleBasedEntityMatcher", "Term"]
